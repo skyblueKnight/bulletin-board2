55d6457906ae134c603d5d65d66b65e791b9aaeb
ayane.tanaka <ayane.tanaka@rakus.co.jp>
2019-06-18 15:49:14 +0900

    記事ドメイン作成

--- src/main/java/com/example/domain/Article.java
+++ src/main/java/com/example/domain/Article.java
@@ -1,5 +1,54 @@
 package com.example.domain;
 
-public class Article {
+import java.util.List;
 
+/**
+ * 記事を保持するドメイン.
+ * 
+ * @author ayane.tanaka
+ *
+ */
+public class Article {
+	/** 記事ID*/
+	private Integer id;
+	/**  投稿者名*/
+	private String name;
+	/** 　記事内容 */
+	private String content;
+	/**	 コメントのリスト */
+	private List<Comment> commentList;
+	
+	
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", name=" + name + ", content=" + content + ", commentList=" + commentList + "]";
+	}
+	
+	//get&set
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public List<Comment> getCommentList() {
+		return commentList;
+	}
+	public void setCommentList(List<Comment> commentList) {
+		this.commentList = commentList;
+	}
+	
+	
 }
