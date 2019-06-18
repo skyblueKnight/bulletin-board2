1308178fce1b918bcac85efc7c03220532d2a832
ayane.tanaka <ayane.tanaka@rakus.co.jp>
2019-06-18 16:21:42 +0900

    記事削除機能実装

--- src/main/java/com/example/controller/DeleteArticleController.java
+++ src/main/java/com/example/controller/DeleteArticleController.java
@@ -1,5 +1,30 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.repository.ArticleRepository;
+
+@Controller
+@RequestMapping("/deleteArticle")
 public class DeleteArticleController {
+	
+	@Autowired
+	private ArticleRepository artrepository;
+	
+	/**
+	 * 指定されたidの記事とその記事へのコメントを削除する.
+	 * 
+	 * @param articleId 指定されたid
+	 * @param model リクエストスコープ
+	 * @return　記事一覧画面
+	 */
+	@RequestMapping("/articledelete")
+	public String articleDelete(Integer articleId,Model model) {
+		artrepository.deleteById(articleId);
+		return "redirect:/";
+	}
 
 }
